76a31ff8acc53b68bfb41f2bfd56cb55dbad2b78
wangwiza <76924804+wangwiza@users.noreply.github.com>
2023-12-19 07:40:46 -0500

    (feat): scraper script file and gitignore

--- src/genshin_fandom_scraper.py
+++ src/genshin_fandom_scraper.py
@@ -1,1 +1,8 @@
-print("Hello world")
+import requests
+from bs4 import BeautifulSoup
+
+
+ROOT_URL = "https://genshin-impact.fandom.com/wiki/"
+
+def extract_character_page(name: str) -> str:
+    pass
