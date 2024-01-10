3574e3c2f506464b2038ff569d06fcc4de736b0f
wangwiza <76924804+wangwiza@users.noreply.github.com>
2024-01-09 23:32:40 -0500

    (feat): clean dialogue lines

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,48 +4,62 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import re
 import html
-import time
+
 
 
 def get_genshin_soup():
     with open(DATA_DIR / XML_FILE, "r", encoding="utf8") as f:
         file = f.read()
-    return BeautifulSoup(file, "lxml").find_all
+    return BeautifulSoup(file, "lxml")
 
 
-def extract_all_characters(soup: BeautifulSoup):
-    with open(f"{DATA_DIR}/characters.txt", "w", encoding="utf8") as f:
-        for elem in soup(text=re.compile(r"{{Character Infobox")):
+def extract_all_titles(soup: BeautifulSoup):
+    with open(f"{DATA_DIR}/titles.txt", "w", encoding="utf8") as f:
+        for elem in soup(text=re.compile(r"{{Dialogue Start}}")):
             page = elem.parent.parent.parent
             character_name = page.title.text
-            if ":" not in character_name:
-                f.write(f"{character_name}\n")
+            f.write(f"{character_name}\n")
 
 
-def extract_all_dialogue_new():
-    with open(f"{DATA_DIR}/{XML_FILE}", "r", encoding="utf8") as xml:
-        with open(f"{DATA_DIR}/dialogues.txt", "w", encoding="utf8") as f:
-            isDialogue = False
-            for line in xml:
-                if "{{Dialogue End}}" in line:
-                    f.write("\n")
-                    isDialogue = False
-                elif "{{Dialogue Start}}" in line:
-                    isDialogue = True
-                elif isDialogue:
-                    f.write(html.unescape(line))
-                else:
-                    pass
-            print("done.")
-
-
-def clean_dialogues():
-    with open(f"{DATA_DIR}/clean_dialogues.txt", "w") as clean_file:
-        with open(f"{DATA_DIR}/dialogues.txt", "r") as dialogues:
-            speaker_pattern = re.compile(r"'''(.+?):''' ")
-            for line in dialogues:
+def extract_all_dialogue():
+    with open(f"{DATA_DIR}/{XML_FILE}", "r", encoding="utf8") as src, open(
+        f"{DATA_DIR}/dialogues.txt", "w", encoding="utf8"
+    ) as dst:
+        isDialogue = False
+        skipping = False
+        for line in src:
+            if '<title>' in line:
+                skipping = any(x in line for x in ('User:', 'File:', 'Template:'))
+                continue
+            if skipping:
+                continue
+            if "{{Dialogue End}}" in line:
+                dst.write("\n")
+                isDialogue = False
+            elif "{{Dialogue Start}}" in line:
+                isDialogue = True
+            elif isDialogue:
+                # dst.write(clean_dialogue_line(line))
+                dst.write(line)
+            else:
                 pass
+        print("done.")
+
+
+def clean_dialogue_line(line: str) -> str:
+    line = line.replace("{{MC|Lumine|Aether|mc=1}}", "Lumine").replace("{{MC|m=Aether|f=Lumine}}", "Aether")
+    if "{{DIcon" in line:
+        start = line.find("}}")+2
+        line_without_prefix = f"Aether: {line[start:]}"
+    else:
+        line_without_prefix = line[line.find("'''"):]
+    unescaped_line = html.unescape(html.unescape(html.unescape(line_without_prefix)))
+    clean_line = unescaped_line.replace("'''", "").replace("[[", "").replace("]]", "").replace("|", ", ")
+    return clean_line
+
 
 
 if __name__ == "__main__":
-    extract_all_dialogue_new()
+    extract_all_dialogue()
+    # soup = get_genshin_soup()
+    # extract_all_titles(soup)
