a14298b8d54e329afde5acb3f71f3e3b303938e7
wangwiza <76924804+wangwiza@users.noreply.github.com>
2024-06-18 08:17:34 -0400

    (fix): improved network view

--- scraper/analysis.py
+++ scraper/analysis.py
@@ -5,10 +5,12 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from pyvis.network import Network
 
+
 # Load into DataFrame
 def load_data_into_df() -> pd.DataFrame:
     with open(f"{DATA_DIR}/exchanges.csv", mode="r", encoding="utf-8") as f:
         return pd.read_csv(f, sep="|")
+
 
 def aggregate_relationships(relationship_df: pd.DataFrame) -> pd.DataFrame:
     relationship_df["value"] = 1
@@ -16,7 +18,9 @@ def aggregate_relationships(relationship_df: pd.DataFrame) -> pd.DataFrame:
         ["source", "target"], sort=False, as_index=False
     ).sum()
 
+
 # Graph analysis and visualization
+
 
 def construct_graph(relationship_df: pd.DataFrame) -> nx.Graph:
     print("Constructing graph...")
@@ -30,24 +34,38 @@ def construct_graph(relationship_df: pd.DataFrame) -> nx.Graph:
     print("Graph constructed.")
     return G
 
+
 def draw_graph(G: nx.Graph):
     print("Getting pos...")
-    # pos = nx.kamada_kawai_layout(G)
+    pos = nx.kamada_kawai_layout(G)
     print("Drawing graph...")
-    nx.draw(G, with_labels=True, node_color="skyblue", edge_cmap=plt.colormaps['Blues'])
+    nx.draw(G, with_labels=True, node_color="skyblue", edge_cmap=plt.colormaps["Blues"])
     print("Displaying graph...")
     plt.show()
     print("Graph displayed.")
 
-def draw_pyvis_graph(G: nx.Graph):
-    net = Network(width="100vw", height="100vh", bgcolor="#222222")
 
-    node_degree = dict(G.degree) # Ignore error
+def draw_pyvis_graph(G: nx.Graph):
+    net = Network(width="100vw", height="100vh", bgcolor="#222222", font_color="white")
+
+    node_degree = dict(G.degree)  # Ignore error
+    node_degree = normalize_values(node_degree, new_min=1, new_max=100)
 
     nx.set_node_attributes(G, node_degree, "size")
 
     net.from_nx(G)
     net.show("genshin_dialogue_graph.html")
+
+
+def normalize_values(d, new_min=1, new_max=100):
+    values = d.values()
+    min_val = min(values)
+    max_val = max(values)
+
+    def normalize(value):
+        return ((value - min_val) / (max_val - min_val)) * (new_max - new_min) + new_min
+
+    return {k: normalize(v) for k, v in d.items()}
 
 
 if __name__ == "__main__":
